95d39e32416a7fa5c47d5a8a5e4e36167bf60520
jerif118 <104994866+jerif118@users.noreply.github.com>
2026-02-22 17:24:47 -0500

    se agrego model training y se cambio el titlo de las graficas, y las etiquetas para la clase binaria

--- tools/labels_json.py
+++ tools/labels_json.py
@@ -1,22 +1,16 @@
 import os
 import json
-
-# 1. Preparamos la lista de etiquetas en el formato de NVIDIA: ["nombre_archivo.jpg", label]
-# Usamos 'label_esp' porque es la que define la patología exacta para la GAN
-def labels_json(df,col_id,label,out_dir):
+def labels_json(df,col_id,label,out_dir,name="dataset.json"):
     labels = [
         [f"{row[col_id]}.jpg", int(row[label])] 
         for _, row in df.iterrows()
     ]
-
-    # 2. Creamos el diccionario con la clave "labels" requerida por StyleGAN
     dataset_json_content = {
         "labels": labels
     }
 
-    out_json = os.path.join(out_dir,"dataset.json")
+    out_json = os.path.join(out_dir,name)
     
-    # 3. Guardamos el archivo
     with open(out_json, 'w', encoding='utf-8') as f:
         json.dump(dataset_json_content, f, indent=4)
     print("Archivo creado.")
